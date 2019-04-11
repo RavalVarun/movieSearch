6a6d7bc682f80dd1c8aa6c7a57ac5d1b3c64918a
Varun <44644972+RavalVarun@users.noreply.github.com>
2019-04-11 17:43:54 -0400

    first commit

--- scripts/JS/script.js
+++ scripts/JS/script.js
@@ -1,21 +1,41 @@
+
 const app = {};
+app.key = '2b14ef1e447db99d2e8042b8470f960b';
 
 app.displayMovie = (movieList) => {
-  
+  movieList.forEach((movie) => {
+    console.log(movie.title);
+    const movieHtml = `<div>
+                      <h2>${movie.original_title}</h2>
+                      
+                      </div>`;
+    $('section').append(movieHtml);
+  })
 }
 
 app.getMovie = (search) => {
+  console.log(`${app.key}`)
   $.ajax({
-
-  }).
+    url:'https://api.themoviedb.org/3/search/movie',
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      api_key: `${app.key}`,
+      query: search
+    }
+  }).then((response)=>{
+    console.log(response.results);
+  })
 }
 
 app.init = () => {
   $('form').on('submit',function(event){
     event.preventDefault();
-
-    const searchTerm = $('#search').val();
+    const searchTerm = $('#search').val()
+    $('#search').val("");
     // $('#search').val("") remove from comments once project is complete
+    console.log(searchTerm);
+    app.getMovie(searchTerm);
   })
 }
 
